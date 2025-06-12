d8268c1de40139ed1c0e28b1f987e5037f9b68df
LOPEZby2006 <diego.lopezm0405@gmail.com>
2025-06-11 22:51:17 -0500

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,16 +1,16 @@
-// src/components/Card.jsx
 import React from "react";
-import "../styles/Dashboard.module.css";
+import styles from "../styles/Dashboard.module.css";
+import { FaArrowRight } from "react-icons/fa";
 
 const Card = ({ image, title, description, link }) => {
     return (
-        <div className="card">
-            {image && <img src={image} alt={title} className="card-img" />}
-            <div className="card-body">
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <a href={link} className="btn">
-                    Ver Más
+        <div className={styles.card}>
+            {image && <img src={image} alt={title} className={styles.cardimg} />}
+            <div className={styles.cardbody}>
+                <h3 className={styles.cardtitle}>{title}</h3>
+                <p className={styles.cardtext}>{description}</p>
+                <a href={link} className={styles.btn}>
+                    Ver Más <FaArrowRight style={{ marginLeft: "8px" }} />
                 </a>
             </div>
         </div>
