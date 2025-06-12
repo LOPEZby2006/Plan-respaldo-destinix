984da2af647613bfd7a78c2f416f7f2665895c65
Dialpi <diegoapirazan@gmail.com>
2025-06-12 03:13:16 -0500

    asd

--- destinix/session.php
+++ destinix/session.php
@@ -1,5 +1,8 @@
 <?php
-header("Access-Control-Allow-Origin: http://ambitious-forest-0ecbd371e.6.azurestaticapps.net");
+<?php
+header("Access-Control-Allow-Origin: https://ambitious-forest-0ecbd371e.6.azurestaticapps.net");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 session_start();
 
